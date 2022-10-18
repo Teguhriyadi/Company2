c7909075b6738bf8b8ed0e42c4dd0efb397c4810
Teguhriyadi <ilham.teguh55@gmail.com>
2022-10-18 23:05:15 +0700

    Menambahkan Field Pada Profil Perusahaan

--- database/seeders/ProfilPerusahaanSeeder.php
+++ database/seeders/ProfilPerusahaanSeeder.php
@@ -17,6 +17,7 @@ class ProfilPerusahaanSeeder extends Seeder
     {
         ProfilPerusahaan::create([
             "profil_nama" => "JLDW",
+            "profil_singkat" => "Murah , Aman dan Terpercaya",
             "profil_deskripsi" => "Lorem ipsum dolor sit amet Lorem Ipsum Dolor Sit Amet",
             "profil_no_hp" => "085324237299",
             "profil_email" => "admin@gmail.com",
