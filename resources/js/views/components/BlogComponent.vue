e039fedd66b510e777fb7902140189bc4b47125e
Teguhriyadi <ilham.teguh55@gmail.com>
2022-10-30 01:35:29 +0700

    Membuat Animasi Loading Saat Data Sedang Diproses

--- resources/js/views/components/BlogComponent.vue
+++ resources/js/views/components/BlogComponent.vue
@@ -9,67 +9,96 @@
             </div>
 
             <div class="row gy-5">
-
-                <div v-for="(artikel, index) in dataArtikel" :key="index" class="col-xl-4 col-md-6" data-aos="fade-up" data-aos-delay="100">
-                    <div class="post-item position-relative h-100">
-                        <div class="post-img position-relative overflow-hidden">
-                            <img :src="artikel.artikel_gambar" class="img-fluid" alt=""/>
-                            <span class="post-date">December 12</span>
-                        </div>
-
-                        <div class="post-content d-flex flex-column">
-                            <h3 class="post-title">
-                                {{ artikel.artikel_judul }}
-                            </h3>
-
-                            <div class="meta d-flex align-items-center">
-                                <div class="d-flex align-items-center">
-                                    <i class="bi bi-person"></i>
-                                    <span class="ps-2">{{ artikel.artikel_created_by }}</span>
-                                </div>
-                                <span class="px-3 text-black-50">/</span>
-                                <div class="d-flex align-items-center">
-                                    <i class="bi bi-folder2"></i>
-                                    <span class="ps-2">{{ artikel.artikel_kategori }}</span>
-                                </div>
+                <template v-if="dataArtikel.length">
+                    <div v-for="(artikel, index) in dataArtikel" :key="index" class="col-xl-4 col-md-6" data-aos="fade-up" data-aos-delay="100">
+                        <div class="post-item position-relative h-100">
+                            <div class="post-img position-relative overflow-hidden">
+                                <img :src="artikel.artikel_gambar" class="img-fluid" alt=""/>
+                                <span class="post-date">December 12</span>
                             </div>
 
-                            <hr />
+                            <div class="post-content d-flex flex-column">
+                                <h3 class="post-title">
+                                    {{ artikel.artikel_judul }}
+                                </h3>
 
-                            <router-link :to="{name: 'blog', params: {slug: artikel.artikel_slug} }" class="readmore stretched-link">
-                                <span>Selengkapnya</span>
-                                <i class="bi bi-arrow-right"></i>
-                            </router-link>
+                                <div class="meta d-flex align-items-center">
+                                    <div class="d-flex align-items-center">
+                                        <i class="bi bi-person"></i>
+                                        <span class="ps-2">{{ artikel.artikel_created_by }}</span>
+                                    </div>
+                                    <span class="px-3 text-black-50">/</span>
+                                    <div class="d-flex align-items-center">
+                                        <i class="bi bi-folder2"></i>
+                                        <span class="ps-2">{{ artikel.artikel_kategori }}</span>
+                                    </div>
+                                </div>
+
+                                <hr />
+
+                                <router-link :to="{name: 'blog', params: {slug: artikel.artikel_slug} }" class="readmore stretched-link">
+                                    <span>Selengkapnya</span>
+                                    <i class="bi bi-arrow-right"></i>
+                                </router-link>
+                            </div>
                         </div>
                     </div>
-                </div>
-
+                </template>
+                <template v-else>
+                    <center v-if="spinner">
+                        <div class="spinner-border" role="status">
+                            <span class="visually-hidden">Loading...</span>
+                        </div>
+                    </center>
+                    <div class="col-md-12" v-if="output">
+                        <div class="alert alert-danger text-center">
+                            <i>
+                                <strong>
+                                    Oopss.. Data Artikel Saat Ini Belum Tersedia
+                                </strong>
+                            </i>
+                        </div>
+                    </div>
+                </template>
             </div>
         </div>
     </section>
 </template>
 
 <script>
-    import axios from "axios"
-    export default {
-        name: "Artikel",
-        data() {
-            return {
-                dataArtikel: []
-            }
-        },
-        created() {
-            this.getArtikel();
-        },
-        methods: {
-            async getArtikel() {
-                try {
-                    const response = await axios.get("artikel");
-                    this.dataArtikel = response.data;
-                } catch (error) {
-                    console.log("Oopss.. Error");
+import axios from "axios"
+export default {
+    name: "Artikel",
+    data() {
+        return {
+            dataArtikel: [],
+            spinner: false,
+            output: false,
+        }
+    },
+    created() {
+        this.getArtikel();
+    },
+    methods: {
+        async getArtikel() {
+            try {
+                this.spinner = true;
+                const response = await axios.get("artikel");
+                if (response.data == "Data Tidak Ada") {
+                    setTimeout(() => {
+                        this.output = true;
+                        this.spinner = false;
+                    }, 1000);
+                } else {
+                    setTimeout(() => {
+                        this.spinner = false;
+                        this.dataArtikel = response.data;
+                    }, 1000);
                 }
+            } catch (error) {
+                console.log("Oopss.. Error");
             }
         }
     }
+}
 </script>
