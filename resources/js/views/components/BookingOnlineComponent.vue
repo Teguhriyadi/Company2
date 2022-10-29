30209c12720cc19ca7bc6e9273adae5bde48ce9e
Teguhriyadi <ilham.teguh55@gmail.com>
2022-10-30 04:15:50 +0700

    Membuat Animasi Loading Saat Data di Proses

--- resources/js/views/components/BookingOnlineComponent.vue
+++ resources/js/views/components/BookingOnlineComponent.vue
@@ -1,6 +1,6 @@
 <template>
     <main id="main">
-        <div class="breadcrumbs d-flex align-items-center" style="background-image: url('theme/img/about.jpg')">
+        <div class="breadcrumbs d-flex align-items-center" style="background-image: url('/desain/img/about.jpg')">
             <div class="container position-relative d-flex flex-column align-items-center" data-aos="fade">
                 <h2>Booking</h2>
                 <ol>
@@ -24,47 +24,78 @@
                         Apabila Anda Terkendala Untuk Datang Ke Tempat. Kami Menyediakan Jasa Layanan Booking Secara Online.
                     </p>
                 </div>
-                <div class="main-timeline">
-                    <div v-for="(booking, index) in dataCaraBooking" :key="index" :class="index % 2 === 0 ? 'timeline left' : 'timeline right'">
-                        <div class="card">
-                            <div class="card-body p-4">
-                                <h3>
-                                    <i :class="booking.booking_icon"></i> {{ booking.booking_judul }}
-                                </h3>
-                                <p class="mb-0">
-                                    {{ booking.booking_deskripsi }}
-                                </p>
+                <template v-if="dataCaraBooking.length">
+                    <div class="main-timeline">
+                        <div v-for="(booking, index) in dataCaraBooking" :key="index" :class="index % 2 === 0 ? 'timeline left' : 'timeline right'">
+                            <div class="card">
+                                <div class="card-body p-4">
+                                    <h3>
+                                        <i :class="booking.booking_icon"></i> {{ booking.booking_judul }}
+                                    </h3>
+                                    <p class="mb-0">
+                                        {{ booking.booking_deskripsi }}
+                                    </p>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
+                </template>
+                <template v-else>
+                    <center v-if="spinner">
+                    <div class="spinner-border" role="status">
+                            <span class="visually-hidden">Loading...</span>
+                        </div>
+                    </center>
+                    <div class="col-md-12" v-if="output">
+                        <div class="alert alert-danger text-center">
+                            <i>
+                                <strong>
+                                    Oopss.. Data Tata Cara Booking Saat Ini Belum Tersedia
+                                </strong>
+                            </i>
+                        </div>
+                    </div>
+                </template>
             </div>
         </section>
     </main>
 </template>
 
 <script>
-    import axios from "axios"
+import axios from "axios"
 
-    export default {
-        name: "BookingOnline",
-        data() {
-            return {
-                dataCaraBooking: []
-            }
-        },
-        created() {
-            this.getCaraBooking();
-        },
-        methods: {
-            async getCaraBooking() {
-                try {
-                    const response = await axios.get("booking_online");
-                    this.dataCaraBooking = response.data;
-                } catch (error) {
-                    console.log("Oopss.. Error");
+export default {
+    name: "BookingOnline",
+    data() {
+        return {
+            dataCaraBooking: [],
+            spinner: false,
+            output: false
+        }
+    },
+    created() {
+        this.getCaraBooking();
+    },
+    methods: {
+        async getCaraBooking() {
+            this.spinner = true;
+            try {
+                const response = await axios.get("booking_online");
+                if (response.data == "Data Tidak Ada") {
+                    setTimeout(() => {
+                        this.output = true;
+                        this.spinner = false;
+                    }, 1000);
+                } else {
+                    setTimeout(() => {
+                        this.dataCaraBooking = response.data;
+                        this.spinner = false;
+                    }, 1000);
                 }
+            } catch (error) {
+                console.log(error);
             }
         }
     }
+}
 </script>
