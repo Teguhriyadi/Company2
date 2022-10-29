ed301b01a81c62d3662cc5f21f3e012dae038271
Teguhriyadi <ilham.teguh55@gmail.com>
2022-10-30 03:52:35 +0700

    Membuat Animasi Loading Saat Data Sedang Load

--- resources/js/views/components/contact/Contact.vue
+++ resources/js/views/components/contact/Contact.vue
@@ -11,37 +11,55 @@
             <div class="row justify-content-center">
                 <div class="col-xl-10">
                     <div class="row">
-                        <div class="col-lg-6">
-                            <div class="row justify-content-center" v-for="(contact, index) in dataContact" :key="index">
-                                <div class="col-md-6 info d-flex flex-column align-items-stretch">
-                                    <i class="fas fa-location"></i>
-                                    <h4>Alamat</h4>
-                                    <p>
-                                        {{ contact.profil_alamat }}
-                                    </p>
-                                </div>
-                                <div class="col-md-6 info d-flex flex-column align-items-stretch">
-                                    <i class="fas fa-phone"></i>
-                                    <h4>Telepon</h4>
-                                    <p>
-                                        {{ contact.profil_no_hp }}
-                                    </p>
-                                </div>
-                                <div class="col-md-6 info d-flex flex-column align-items-stretch">
-                                    <i class="fas fa-envelope"></i>
-                                    <h4>Email</h4>
-                                    <p>{{ contact.profil_email }}</p>
-                                </div>
-                                <div class="col-md-6 info d-flex flex-column align-items-stretch">
-                                    <i class="fas fa-times-circle"></i>
-                                    <h4>Jam Kerja</h4>
-                                    <p>
-                                        Senin - Jumat : 08.00 - 16.00
-                                    </p>
+
+                        <template v-if="dataContact.length">
+                            <div class="col-lg-6">
+                                <div class="row justify-content-center" v-for="(contact, index) in dataContact" :key="index">
+                                    <div class="col-md-6 info d-flex flex-column align-items-stretch">
+                                        <i class="fas fa-location"></i>
+                                        <h4>Alamat</h4>
+                                        <p>
+                                            {{ contact.profil_alamat }}
+                                        </p>
+                                    </div>
+                                    <div class="col-md-6 info d-flex flex-column align-items-stretch">
+                                        <i class="fas fa-phone"></i>
+                                        <h4>Telepon</h4>
+                                        <p>
+                                            {{ contact.profil_no_hp }}
+                                        </p>
+                                    </div>
+                                    <div class="col-md-6 info d-flex flex-column align-items-stretch">
+                                        <i class="fas fa-envelope"></i>
+                                        <h4>Email</h4>
+                                        <p>{{ contact.profil_email }}</p>
+                                    </div>
+                                    <div class="col-md-6 info d-flex flex-column align-items-stretch">
+                                        <i class="fas fa-times-circle"></i>
+                                        <h4>Jam Kerja</h4>
+                                        <p>
+                                            Senin - Jumat : 08.00 - 16.00
+                                        </p>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-
+                        </template>
+                        <template v-else>
+                            <div class="col-lg-6">
+                                <center v-if="spinner">
+                                    <div class="spinner-border" role="status">
+                                        <span class="visually-hidden">Loading...</span>
+                                    </div>
+                                </center>
+                                <div class="alert alert-danger text-center" v-if="output">
+                                    <i>
+                                        <strong>
+                                            Oopss.. Data Profil Perusahaan Saat Ini Belum Tersedia
+                                        </strong>
+                                    </i>
+                                </div>
+                            </div>
+                        </template>
                         <div class="col-lg-6">
                             <form method="post" role="form" class="php-email-form" @submit.prevent="kirimPesan">
                                 <div class="row">
@@ -66,7 +84,7 @@
                                     <button type="submit" class="button-kirim">
                                         <span v-if="loading">
                                             <i>
-                                               Form Isian Anda Sedang Diproses
+                                                Form Isian Anda Sedang Diproses
                                             </i>
                                         </span>
                                         <span v-else>
@@ -84,58 +102,71 @@
 </template>
 
 <style scoped>
-    .button-kirim {
-        width: 100%;
-        padding: 5px 10px 5px 10px;
-        font-size: 14px;
-        font-weight: bold;
-    }
+.button-kirim {
+    width: 100%;
+    padding: 5px 10px 5px 10px;
+    font-size: 14px;
+    font-weight: bold;
+}
 
 </style>
 
 <script>
-    import axios from "axios"
-    export default {
-        name: "Contact",
-        data() {
-            return {
-                dataContact: [],
-                pesan: [],
-                loading: false
+import axios from "axios"
+export default {
+    name: "Contact",
+    data() {
+        return {
+            dataContact: [],
+            pesan: [],
+            loading: false,
+            spinner: false,
+            output: false,
+        }
+    },
+    created() {
+        this.getContact();
+    },
+    methods: {
+        async getContact() {
+            this.spinner = true;
+            try {
+                const response = await axios.get("profil_perusahaan");
+                if (response.data[0].message == "Data Tidak Ada") {
+                    setTimeout(() => {
+                        this.output = true;
+                        this.spinner = false;
+                    }, 1000);
+                } else {
+                    setTimeout(() => {
+                        this.dataContact = response.data;
+                        this.spinner = false;
+                    }, 1000);
+                }
+            } catch (error) {
+                console.log("Oopss.. Error");
             }
         },
-        created() {
-            this.getContact();
-        },
-        methods: {
-            async getContact() {
-                try {
-                    const response = await axios.get("profil_perusahaan");
-                    this.dataContact = response.data;
-                } catch (error) {
-                    console.log("Oopss.. Error");
-                }
-            },
 
-            kirimPesan() {
-                if (this.pesan.nama && this.pesan.email && this.pesan.subjek && this.pesan.teks) {
-                    axios.post("pesan", {
-                        pesan_nama: this.pesan.nama,
-                        pesan_email: this.pesan.email,
-                        pesan_subjek: this.pesan.subjek,
-                        pesan_teks: this.pesan.teks
-                    }).then(response => {
-                        this.loading = true;
-                        setTimeout(() => {
-                            this.loading = false;
-                            alert("Data Berhasil di Tambahkan");
-                            window.location = '/'
-                        }, 1000);
-                    }).catch(error => {
-                        alert("Harap Isi Form Dengan Benar");
-                    });
-                }
+        kirimPesan() {
+            if (this.pesan.nama && this.pesan.email && this.pesan.subjek && this.pesan.teks) {
+                axios.post("pesan", {
+                    pesan_nama: this.pesan.nama,
+                    pesan_email: this.pesan.email,
+                    pesan_subjek: this.pesan.subjek,
+                    pesan_teks: this.pesan.teks
+                }).then(response => {
+                    this.loading = true;
+                    setTimeout(() => {
+                        this.loading = false;
+                        alert("Data Berhasil di Tambahkan");
+                        window.location = '/'
+                    }, 1000);
+                }).catch(error => {
+                    alert("Harap Isi Form Dengan Benar");
+                });
             }
         }
     }
+}
 </script>
