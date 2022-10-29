e1381a41173f864c7a5064f167ea575ec29dc4f9
Teguhriyadi <ilham.teguh55@gmail.com>
2022-10-30 01:49:28 +0700

    Membuat Animasi Loading Saat Data Tidak Ada

--- resources/js/views/components/TestimonialComponent.vue
+++ resources/js/views/components/TestimonialComponent.vue
@@ -7,29 +7,47 @@
                     Beberapa Orang Yang Telah Kami Jadikan Sebagai Testimonial Setelah Menggunakan Jasa Kami
                 </p>
             </div>
-            <swiper :modules="modules" :pagination="{ clickable: true }">
-                <swiper-slide v-for="(testimonial, index) in dataTestimonial" :key="index">
-                    <div class="testimonial-wrap">
-                        <div class="testimonial-item">
-                            <img :src="testimonial.testimonial_foto" class="testimonial-img" alt=""/>
-                            <h3>{{ testimonial.testimonial_nama }}</h3>
-                            <h4>{{ testimonial.testimonial_jobtitle }}</h4>
-                            <div class="stars">
-                                <i class="bi bi-star-fill"></i>
-                                <i class="bi bi-star-fill"></i>
-                                <i class="bi bi-star-fill"></i>
-                                <i class="bi bi-star-fill"></i>
-                                <i class="bi bi-star-fill"></i>
+            <template v-if="dataTestimonial.length">
+                <swiper :modules="modules" :pagination="{ clickable: true }">
+                    <swiper-slide v-for="(testimonial, index) in dataTestimonial" :key="index">
+                        <div class="testimonial-wrap">
+                            <div class="testimonial-item">
+                                <img :src="testimonial.testimonial_foto" class="testimonial-img" alt=""/>
+                                <h3>{{ testimonial.testimonial_nama }}</h3>
+                                <h4>{{ testimonial.testimonial_jobtitle }}</h4>
+                                <div class="stars">
+                                    <i class="bi bi-star-fill"></i>
+                                    <i class="bi bi-star-fill"></i>
+                                    <i class="bi bi-star-fill"></i>
+                                    <i class="bi bi-star-fill"></i>
+                                    <i class="bi bi-star-fill"></i>
+                                </div>
+                                <p>
+                                    <i class="bi bi-quote quote-icon-left"></i>
+                                    {{ testimonial.testimonial_review }}
+                                    <i class="bi bi-quote quote-icon-right"></i>
+                                </p>
                             </div>
-                            <p>
-                                <i class="bi bi-quote quote-icon-left"></i>
-                                {{ testimonial.testimonial_review }}
-                                <i class="bi bi-quote quote-icon-right"></i>
-                            </p>
                         </div>
+                    </swiper-slide>
+                </swiper>
+            </template>
+            <template v-else>
+                <center v-if="spinner">
+                    <div class="spinner-border" role="status">
+                        <span class="visually-hidden">Loading...</span>
                     </div>
-                </swiper-slide>
-            </swiper>
+                </center>
+                <div class="col-md-12" v-if="output">
+                    <div class="alert alert-danger text-center">
+                        <i>
+                            <strong>
+                                Oopss.. Data Testimonial Saat Ini Belum Tersedia
+                            </strong>
+                        </i>
+                    </div>
+                </div>
+            </template>
         </div>
     </section>
 </template>
@@ -52,7 +70,9 @@ export default {
     },
     data() {
         return {
-            dataTestimonial: []
+            dataTestimonial: [],
+            spinner: false,
+            output: false
         }
     },
     created() {
@@ -61,8 +81,19 @@ export default {
     methods: {
         async getTestimonial() {
             try {
+                this.spinner = true;
                 const response = await axios.get("testimonial");
-                this.dataTestimonial = response.data;
+                if (response.data == "Data Tidak Ada") {
+                    setTimeout(() => {
+                        this.output = true;
+                        this.spinner = false;
+                    }, 1000);
+                } else {
+                    setTimeout(() => {
+                        this.dataTestimonial = response.data;
+                        this.spinner = false;
+                    }, 1000);
+                }
             } catch (error) {
                 console.log("Oopss.. Error");
             }
--- resources/js/views/components/jasa/Jasa.vue
+++ resources/js/views/components/jasa/Jasa.vue
@@ -9,45 +9,74 @@
             </div>
 
             <div class="row gy-4">
-
-                <div v-for="(jasa, index) in dataServices" :key="index" class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="100">
-                    <div class="service-item position-relative">
-                        <div class="icon">
-                            <i class="fas fa-location"></i>
+                <template v-if="dataServices.length">
+                    <div v-for="(jasa, index) in dataServices" :key="index" class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="100">
+                        <div class="service-item position-relative">
+                            <div class="icon">
+                                <i class="fas fa-location"></i>
+                            </div>
+                            <h3>{{ jasa.services_title }}</h3>
+                            <p>
+                                {{ jasa.services_deskripsi }}
+                            </p>
                         </div>
-                        <h3>{{ jasa.services_title }}</h3>
-                        <p>
-                            {{ jasa.services_deskripsi }}
-                        </p>
                     </div>
-                </div>
-
+                </template>
+                <template v-else>
+                    <center v-if="spinner">
+                        <div class="spinner-border" role="status">
+                            <span class="visually-hidden">Loading...</span>
+                        </div>
+                    </center>
+                    <div class="col-md-12" v-if="output">
+                        <div class="alert alert-danger text-center">
+                            <i>
+                                <strong>
+                                    Oopss.. Data Jasa Saat Ini Belum Tersedia
+                                </strong>
+                            </i>
+                        </div>
+                    </div>
+                </template>
             </div>
         </div>
     </section>
 </template>
 
 <script>
-    import axios from "axios";
-    export default {
-        name: "Services",
-        data() {
-            return {
-                dataServices: []
-            }
-        },
-        created() {
-            this.getServices();
-        },
-        methods: {
-            async getServices() {
-                try {
-                    const response = await axios.get("services");
-                    this.dataServices = response.data;
-                } catch (error) {
-                    console.log("Oopss.. Error");
+import axios from "axios";
+export default {
+    name: "Services",
+    data() {
+        return {
+            dataServices: [],
+            spinner: false,
+            output: false
+        }
+    },
+    created() {
+        this.getServices();
+    },
+    methods: {
+        async getServices() {
+            try {
+                this.spinner = true;
+                const response = await axios.get("services");
+                if (response.data == "Data Tidak Ada") {
+                    setTimeout(() => {
+                        this.output = true;
+                        this.spinner = false;
+                    }, 1000);
+                } else {
+                    setTimeout(() => {
+                        this.dataServices = response.data;
+                        this.spinner = false;
+                    }, 1000);
                 }
+            } catch (error) {
+                console.log("Oopss.. Error");
             }
         }
     }
+}
 </script>
