4896d1b187c1db3f5cd6f80fd0402e6ddf97b48b
Teguhriyadi <ilham.teguh55@gmail.com>
2022-10-30 15:17:10 +0700

    Membuat Data Dummy BenefitJasa

--- database/seeders/Jasa/BenefitJasaSeeder.php
+++ database/seeders/Jasa/BenefitJasaSeeder.php
@@ -29,5 +29,40 @@ class BenefitJasaSeeder extends Seeder
             "benefit_nama" => "Berkualitas",
             "produk_id" => 4
         ]);
+
+        BenefitJasa::create([
+            "benefit_nama" => "Bersih",
+            "produk_id" => 8
+        ]);
+
+        BenefitJasa::create([
+            "benefit_nama" => "Mengkilat",
+            "produk_id" => 8
+        ]);
+
+        BenefitJasa::create([
+            "benefit_nama" => "Terpercaya",
+            "produk_id" => 8
+        ]);
+
+        BenefitJasa::create([
+            "benefit_nama" => "Kilau",
+            "produk_id" => 10
+        ]);
+
+        BenefitJasa::create([
+            "benefit_nama" => "Keren",
+            "produk_id" => 10
+        ]);
+
+        BenefitJasa::create([
+            "benefit_nama" => "Rapih",
+            "produk_id" => 11
+        ]);
+
+        BenefitJasa::create([
+            "benefit_nama" => "Rata",
+            "produk_id" => 9
+        ]);
     }
 }
