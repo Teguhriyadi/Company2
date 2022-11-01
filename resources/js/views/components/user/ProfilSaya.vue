af065d62e53a1e2bde0273a77598cfde873c6c12
Teguhriyadi <ilham.teguh55@gmail.com>
2022-11-01 14:18:37 +0700

    Membuat Desain Profile

--- resources/js/views/components/user/ProfilSaya.vue
+++ resources/js/views/components/user/ProfilSaya.vue
@@ -1,47 +1,145 @@
 <template>
     <main id="main">
+        <div class="breadcrumbs d-flex align-items-center" style="background-image: url('/theme/img/about.jpg')">
+            <div class="container position-relative d-flex flex-column align-items-center" data-aos="fade">
+                <h2>Profil Saya</h2>
+                <ol>
+                    <li>
+                        <router-link to="/">
+                            Home
+                        </router-link>
+                    </li>
+                    <li>
+                        Update Profil Saya
+                    </li>
+                </ol>
+            </div>
+        </div>
         <div class="container rounded bg-white mt-5 mb-5">
-        <div class="card">
-            <div class="row">
-                <div class="col-md-3 border-right">
-                    <div class="d-flex flex-column align-items-center text-center p-3 py-5">
-                        <img class="rounded-circle mt-5" width="150px"
-                        src="https://st3.depositphotos.com/15648834/17930/v/600/depositphotos_179308454-stock-illustration-unknown-person-silhouette-glasses-profile.jpg">
-                        <span class="font-weight-bold">Edogaru</span>
-                        <span class="text-black-50">edogaru@mail.com.my</span>
-                        <span> </span>
+            <div class="card">
+                <div class="row">
+                    <div class="col-md-3 border-right">
+                        <div class="d-flex flex-column align-items-center text-center p-3 py-5">
+
+                            <img v-if="!user.foto" class="rounded-circle mt-5 img-fluid" style="width: 50%;"
+                            :src="user.foto">
+                            <img v-else class="rounded-circle mt-5 img-fluid" style="width: 50%;" :src="user.foto">
+                            <span class="font-weight-bold">
+                                {{ user.first_name }} {{ user.last_name }}
+                            </span>
+                            <span class="text-black-50">
+                                {{ user.email }}
+                            </span>
+                            <span> </span>
+                        </div>
                     </div>
-                </div>
-                <div class="col-md-5 border-right">
-                    <div class="p-3 py-5">
-                        <div class="d-flex justify-content-between align-items-center mb-3">
-                            <h4 class="text-right">Informasi Profil</h4>
-                        </div>
-                        <div class="row mt-2">
-                            <div class="col-md-6"><label class="labels">First Name</label><input type="text" class="form-control" placeholder="first name" value=""></div>
-                            <div class="col-md-6"><label class="labels">Last Name</label><input type="text" class="form-control" value="" placeholder="surname"></div>
-                        </div>
-                        <div class="row mt-3">
-                            <div class="col-md-12"><label class="labels">Mobile Number</label><input type="number" class="form-control" placeholder="enter phone number" value=""></div>
-                            <div class="col-md-12"><label class="labels">Address Line 1</label><input type="text" class="form-control" placeholder="enter address line 1" value=""></div>
-                            <div class="col-md-12"><label class="labels">Email ID</label><input type="text" class="form-control" placeholder="enter email id" value=""></div>
-                        </div>
-                        <div class="row mt-3">
-                            <div class="col-md-6"><label class="labels">Country</label><input type="text" class="form-control" placeholder="country" value=""></div>
-                            <div class="col-md-6"><label class="labels">State/Region</label><input type="text" class="form-control" value="" placeholder="state"></div>
-                        </div>
-                        <div class="mt-5 text-center"><button class="btn btn-primary profile-button" type="button">Save dan update Profile</button></div>
-                    </div>
-                </div>
-                <div class="col-md-4">
-                    <div class="p-3 py-5">
-                        <div class="d-flex justify-content-between align-items-center experience"><span>Informasi Akun</span></div><br>
-                        <div class="col-md-12"><label class="labels">Password</label><input type="password" class="form-control" placeholder="Password Saat Ini" value=""></div> <br>
-                        <div class="col-md-12"><label class="labels">New Password</label><input type="password" class="form-control" placeholder="Password Terbaru" value=""></div>
+                    <div class="col-md-8">
+                        <form method="POST" @submit.prevent="updateProfil">
+                            <div class="p-3 py-5">
+                                <div class="d-flex justify-content-between align-items-center mb-3">
+                                    <h4 class="text-right">
+                                        Informasi Akun Profil Saya
+                                    </h4>
+                                </div>
+                                <div class="row mt-2">
+                                    <div class="col-md-6">
+                                        <label class="labels">
+                                            Nama Depan
+                                        </label>
+                                        <input type="text" class="form-control" placeholder="Masukkan Nama Depan Anda" v-model="form.first_name">
+                                    </div>
+                                    <div class="col-md-6">
+                                        <label class="labels">
+                                            Nama Belakang
+                                        </label>
+                                        <input type="text" class="form-control" v-model="form.last_name" placeholder="Masukkan Nama Belakang Anda">
+                                    </div>
+                                </div>
+                                <div class="row mt-3">
+                                    <div class="col-md-12">
+                                        <label class="labels">
+                                            Nomor HP
+                                        </label>
+                                        <input type="number" class="form-control" placeholder="Masukkan Nomor Anda" value="081214707143">
+                                    </div>
+                                </div>
+                                <div class="row mt-3">
+                                    <div class="col-md-12">
+                                        <label class="labels">
+                                            Alamat
+                                        </label>
+                                        <input type="text" class="form-control" placeholder="Masukkan Alamat Anda" value="Villa Intan 2 Blok i4, No.1. Cirebon, Jawa Barat">
+                                    </div>
+                                </div>
+                                <div class="row mt-3">
+                                    <div class="col-md-12">
+                                        <label class="labels">
+                                            Email
+                                        </label>
+                                        <input type="text" class="form-control" placeholder="Masukkan Email Anda" v-model="form.email" readonly>
+                                    </div>
+                                </div>
+                                <div class="row mt-3">
+                                    <div class="col-md-6">
+                                        <label class="labels">
+                                            Kota / Kabupaten
+                                        </label>
+                                        <input type="text" class="form-control" placeholder="country" value="Kota Cirebon">
+                                    </div>
+                                    <div class="col-md-6">
+                                        <label class="labels">
+                                            Kecamatan
+                                        </label>
+                                        <input type="text" class="form-control" value="Lemahwungkuk" placeholder="state">
+                                    </div>
+                                </div>
+                                <div class="mt-5 mb-5 text-center">
+                                    <button type="reset" class="btn btn-danger" style="margin-right: 5px;">
+                                        Batal
+                                    </button>
+                                    <button class="btn btn-success profile-button" type="submit">
+                                        Simpan
+                                    </button>
+                                </div>
+                            </div>
+                        </form>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
     </main>
 </template>
+
+<script>
+import axios from "axios"
+export default {
+    name: "UpdateProfil",
+    data() {
+        return {
+            loggedIn: localStorage.getItem("loggedIn"),
+            token: localStorage.getItem("token"),
+            user: [],
+            form: {
+                first_name: "",
+                last_name: "",
+                email: ""
+            }
+        }
+    },
+    created() {
+        axios.get("/user", {
+            headers: {
+                'Authorization': 'Bearer ' + this.token
+            }
+        }).then(response => {
+            this.user = response.data
+            let post = response.data
+            this.form = {
+                first_name: post.first_name,
+                last_name: post.last_name,
+                email: post.email
+            };
+        })
+    },
+}
+</script>
