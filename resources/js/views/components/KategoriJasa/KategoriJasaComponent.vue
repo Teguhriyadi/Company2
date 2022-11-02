c9ebe61889279a2667bd964c25af668f51fd9600
Teguhriyadi <ilham.teguh55@gmail.com>
2022-11-02 15:08:42 +0700

    FIX Bugging

--- resources/js/views/components/KategoriJasa/KategoriJasaComponent.vue
+++ resources/js/views/components/KategoriJasa/KategoriJasaComponent.vue
@@ -1,6 +1,6 @@
 <template>
     <main id="main">
-        <div class="breadcrumbs d-flex align-items-center" style="background-image: url('/desain/img/about.jpg')">
+        <div class="breadcrumbs d-flex align-items-center" style="background-image: url('/theme/img/about.jpg')">
             <div class="container position-relative d-flex flex-column align-items-center" data-aos="fade">
                 <h2>Kategori {{ namaJasa }} </h2>
                 <ol>
@@ -25,58 +25,76 @@
                             Kami Menyediakan Beberapa Jasa Harga Yang Bisa Anda Pilih Sesuai Dengan Kebutuhan
                         </p>
                     </div>
+                    <div class="row mb-3">
+                        <div class="col-md-10">
+                            <div class="alert alert-warning" role="alert">
+                                <p class="mb-0">Sebelum memesan, harap konsultasi terlebih dahulu ya ke admin kami</p>
+                            </div>
+                        </div>
+                        <div class="col-md-2 pt-2">
+                            <a target="_blank" href="https://wa.me/6281411126356?text=Hai%20Admin" class="btn btn-success" style="width: 100%;">
+                                <i class="fa fa-phone"></i> Hubungi admin
+                            </a>
+                        </div>
+                    </div>
                     <div class="row" data-aos="fade-left">
-                        <template v-for="(produk, index) in dataProdukPaket" :key="index">
-                            <div v-if="idJasa == produk.produk_paket_jasa_id" class="col-lg-3 col-md-6 mb-3">
-                                <div class="box" data-aos="zoom-in" data-aos-delay="100">
-                                    <h3>{{ produk.produk_paket_nama }}</h3>
-                                    <h4>
-                                        <sup>Rp</sup>
-                                        <span style="color: orange; font-size: 20px; font-weight:">{{ produk.harga }} / Project</span>
-                                    </h4>
-                                    <ul>
-                                        <template v-if="dataBenefit.length">
-                                            <template v-for="(benefit, index) in dataBenefit" :key="index">
-                                                <li v-if="benefit.benefit_produk_id == produk.produk_id">
-                                                    {{ benefit.benefit_nama }}
-                                                </li>
-                                                <li v-else class="na">
-                                                    {{ benefit.benefit_nama }}
-                                                </li>
+                        <template v-if="dataProdukPaket.length">
+                            <template v-for="(produk, index) in dataProdukPaket" :key="index">
+                                <div v-if="idJasa == produk.produk_paket_jasa_id" class="col-lg-3 col-md-6 mb-3">
+                                    <div class="box" data-aos="zoom-in" data-aos-delay="100">
+                                        <h3>{{ produk.produk_paket_nama }}</h3>
+                                        <h4>
+                                            <sup>Rp</sup>
+                                            <span style="color: orange; font-size: 20px; font-weight:">{{ produk.harga }} / Project</span>
+                                        </h4>
+                                        <ul>
+                                            <template v-if="dataBenefit.length">
+                                                <template v-for="(benefit, index) in dataBenefit" :key="index">
+                                                    <li v-if="benefit.benefit_produk_id == produk.produk_id">
+                                                        {{ benefit.benefit_nama }}
+                                                    </li>
+                                                    <li v-else class="na">
+                                                        {{ benefit.benefit_nama }}
+                                                    </li>
+                                                </template>
                                             </template>
-                                        </template>
-                                        <template v-else>
-                                            <center v-if="spinner">
-                                                <div class="spinner-border" role="status">
-                                                    <span class="visually-hidden">Loading...</span>
+                                            <template v-else>
+                                                <center v-if="spinner">
+                                                    <div class="spinner-border" role="status">
+                                                        <span class="visually-hidden">Loading...</span>
+                                                    </div>
+                                                </center>
+                                                <div class="alert alert-danger text-center" v-if="output">
+                                                    <i>
+                                                        <strong style="color: white;">
+                                                            Benefit Belum Tersedia
+                                                        </strong>
+                                                    </i>
                                                 </div>
-                                            </center>
-                                            <div class="alert alert-danger text-center" v-if="output">
-                                                <i>
-                                                    <strong style="color: white;">
-                                                        Benefit Belum Tersedia
-                                                    </strong>
-                                                </i>
-                                            </div>
-                                        </template>
-                                    </ul>
-                                    <div class="btn-wrap">
-                                        <template v-if="!user.nama">
-                                            <a class="btn-buy" @click="autentikasi">
-                                                Booking Sekarang
-                                            </a>
-                                        </template>
-                                        <template v-else>
-                                            <a class="btn-buy" data-bs-toggle="modal" data-bs-target="#exampleModal" @click="booking(produk.produk_id)">
-                                                Booking Sekarang
-                                            </a>
-                                            <a class="btn btn-success" @click="buttonKlik" >
-                                                Booking
-                                            </a>
-                                        </template>
+                                            </template>
+                                        </ul>
+                                        <div class="btn-wrap">
+                                            <template v-if="!user.nama">
+                                                <a class="btn-buy" @click="autentikasi">
+                                                    Booking Sekarang
+                                                </a>
+                                            </template>
+                                            <template v-else>
+                                                <a class="btn-buy" data-bs-toggle="modal" data-bs-target="#exampleModal" @click="booking(produk.produk_id)">
+                                                    Booking Sekarang
+                                                </a>
+                                            </template>
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
+                            </template>
+                        </template>
+                        <template v-else>
+                            <center v-if="spinner">
+                                <div class="spinner-border" role="status">
+                                    <span class="visually-hidden">Loading...</span>
+                                </div>
+                            </center>
                         </template>
                     </div>
                 </div>
@@ -123,7 +141,7 @@
                         </div>
                     </template>
                     <template v-else>
-                        <center v-if="spinner">
+                        <center v-if="load">
                             <div class="spinner-border" role="status">
                                 <span class="visually-hidden">Loading...</span>
                             </div>
@@ -202,6 +220,7 @@ export default {
             loggedIn: localStorage.getItem("loggedIn"),
             token: localStorage.getItem("token"),
             user: [],
+            load: false
         }
     },
     created() {
@@ -234,10 +253,13 @@ export default {
         },
 
         async getProdukPaket() {
+            this.load = true;
             try {
                 const response = await axios.get("produk_paket");
-                console.log(response.data);
-                this.dataProdukPaket = response.data;
+                setTimeout(() => {
+                    this.dataProdukPaket = response.data;
+                    this.load = false;
+                }, 500);
             } catch (error) {
                 console.log("Oopss.. Error");
             }
@@ -278,12 +300,6 @@ export default {
             window.location = "/login";
         },
 
-        buttonKlik() {
-            setTimeout(() => {
-                window.open("https://wa.me/6281411126356?text=Hallo", "_blank");
-            }, 500);
-        },
-
         booking(produk_id) {
             this.produkId = produk_id;
 
@@ -299,10 +315,41 @@ export default {
                     if (response.data == "Data Tidak Ada") {
                         alert("Kode Yang Anda Masukkan Salah");
                     } else {
-                        if (response.data[0].status == 1) {
+                        if (response.data[0].jasa_id == this.idJasa) {
+                            if (response.data[0].status == 1) {
+                                axios.post("jasa_token", {
+                                    kode: kode,
+                                    user_id: this.user.id,
+                                    created_by: response.data[0].created_by,
+                                    jasa_id: this.idJasa,
+                                    produk_id: this.produkId
+                                }).then(response => {
+                                    if (response.data.success == 1) {
 
+                                        setTimeout(() => {
+
+                                            alert("Token Diterima, Anda Akan Diarahkan Ke Halaman Selanjutnya");
+                                            $(".modal").modal("hide");
+                                            return this.$router.push(
+                                            {
+                                                name: 'cart',
+                                                params:
+                                                {
+                                                    produk_id: this.produkId,
+                                                    nama_jasa: this.namaJasa
+                                                }
+                                            },
+                                            );
+                                        }, 1000);
+                                    }
+                                }).catch(error => {
+                                    alert(error);
+                                });
+                            } else {
+                                alert("Kode Verifikasi Tidak Aktif");
+                            }
                         } else {
-                            alert("Kode Verifikasi Tidak Aktif");
+                            alert("Token Hanya Dapat Digunakan Pada Jasa Yang Sesuai");
                         }
                     }
                 }).catch(error => {
