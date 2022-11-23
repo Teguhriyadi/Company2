e1ee8fcfd119de80354e78b161855606915df6fd
Teguhriyadi <ilham.teguh55@gmail.com>
2022-11-23 17:05:40 +0700

    FIX

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,6 @@
 <?php
 
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: *');
-header('Access-Control-Allow-Headers: *');
+    header('Access-Control-Allow-Origin: *');
 
 use App\Http\Controllers\API\AboutUsController;
 use App\Http\Controllers\API\CarouselController;
