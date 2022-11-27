98b72190e1ae920777a2b70e5865cc8dde4807f6
Teguhriyadi <ilham.teguh55@gmail.com>
2022-11-28 01:27:36 +0700

    Memperbaiki Card Counting Dashboard

--- app/Http/Controllers/AppController.php
+++ app/Http/Controllers/AppController.php
@@ -6,6 +6,7 @@ use App\Models\Akun\InformasiLogin;
 use App\Models\Master\Portfolio;
 use App\Models\Pengaturan\ProfilPerusahaan;
 use App\Models\Pengaturan\Testimonial;
+use App\Models\Transaksi\KodeToken;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -19,7 +20,8 @@ class AppController extends Controller
             "data_last_login" => InformasiLogin::where("id_user", Auth::user()->id)->orderBy("created_at", "DESC")->paginate(5),
             "count_testi" => Testimonial::count(),
             "count_portfolio" => Portfolio::count(),
-            "count_akun" => User::where("status", 1)->count()
+            "count_akun" => User::where("status", 1)->count(),
+            "count_aktivasi" => KodeToken::where("status", 0)->count()
         ];
 
         return view("pages.admin.dashboard", $data);
